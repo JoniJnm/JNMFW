a761fee0dcb3b1b5c57d95fc6fa16ec25cd393e8
JoniJnm <joni@jonijnm.es>
2014-06-14 19:12:01 +0200

    Permitir definir ruta del archivo de log

--- jnmfw/helpers/HLog.php
+++ jnmfw/helpers/HLog.php
@@ -3,19 +3,31 @@
 namespace JNMFW\helpers;
 
 abstract class HLog {
-	static public function logError($msg) {
-		echo $msg." <br />\n";
+	static private $file;
+	
+	static public function setLogFile($file) {
+		static::$file = $file;
 	}
 	
-	static public function logDebug($msg) {
-		
+	static public function logError($msg) {
+		static::log('ERROR', $msg);
 	}
 	
 	static public function logWarning($msg) {
-		
+		static::log('WARNING', $msg);
+	}
+	
+	static public function logDebug($msg) {
+		static::log('DEBUG', $msg);
 	}
 	
 	static public function logVerbose($msg) {
-		
+		static::log('VERBOSE', $msg);
+	}
+	
+	static private function log($label, $msg) {
+		$log = date('d-m H:i:s').' - '.$label.' - '.$msg;
+		if (static::$file) file_put_contents(static::$file, $log."\n", FILE_APPEND);
+		else echo $log." <br />\n";
 	}
 }
