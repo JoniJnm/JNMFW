3c34f1bd0ef650e0f9f71ae04511f961f2c55fe2
JoniJnm <joni@jonijnm.es>
2014-08-03 17:58:26 +0200

    Permitir setear nivel de error

--- jnmfw/helpers/HLog.php
+++ jnmfw/helpers/HLog.php
@@ -3,10 +3,21 @@
 namespace JNMFW\helpers;
 
 abstract class HLog {
+	const LEVEL_NONE = 0;
+	const LEVEL_VERBOSE = 1;
+	const LEVEL_DEBUG = 2;
+	const LEVEL_WARNING = 3;
+	const LEVLE_ERROR = 4;
+	
+	static private $level = 2;
 	static private $file;
 	
 	static public function setLogFile($file) {
 		static::$file = $file;
+	}
+	
+	static public function setLevelError($level) {
+		static::$level = $level;
 	}
 	
 	static public function logError($msg) {
@@ -17,22 +28,23 @@ abstract class HLog {
 			else
 				$trace[] = $t['function'].'()';
 		}
-		static::log('ERROR', $msg.' - '.implode(', ', $trace));
+		static::log(static::LEVEL_ERROR, 'ERROR', $msg.' - '.implode(', ', $trace));
 	}
 	
 	static public function logWarning($msg) {
-		static::log('WARNING', $msg);
+		static::log(static::LEVEL_WARNING, 'WARNING', $msg);
 	}
 	
 	static public function logDebug($msg) {
-		static::log('DEBUG', $msg);
+		static::log(static::LEVEL_DEBUG, 'DEBUG', $msg);
 	}
 	
 	static public function logVerbose($msg) {
-		static::log('VERBOSE', $msg);
+		static::log(static::LEVEL_VERBOSE, 'VERBOSE', $msg);
 	}
 	
-	static private function log($label, $msg) {
+	static private function log($level, $label, $msg) {
+		if ($level < static::$level) return;
 		$log = date('d-m H:i:s').' - '.$label.' - '.$msg;
 		if (static::$file) file_put_contents(static::$file, $log."\n", FILE_APPEND);
 		else echo $log." <br />\n";
