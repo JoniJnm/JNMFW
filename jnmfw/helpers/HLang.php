3c7064edf86aca4500944d824f814673c3b0f423
JoniJnm <joni@jonijnm.es>
2014-08-15 17:23:55 +0200

    Poder exportar archivos de idioma a carpeta

--- jnmfw/helpers/HLang.php
+++ jnmfw/helpers/HLang.php
@@ -26,4 +26,25 @@ abstract class HLang {
 			return $key;
 		}
 	}
+	
+	public static function getUserLang() {
+		return static::$lang;
+	}
+	
+	public static function toJS($folder) {
+		$filename = strtolower(static::$lang).'-'.
+				constant('\\'.static::$namespace.'\Lang'.static::$lang.'::VERSION').'-'.
+				constant('\\'.static::$namespace.'\Lang'.static::$default.'::VERSION').'.js';
+		$file = $folder.'/'.$filename;
+		if (file_exists($file)) return $filename;
+		$class = '\\'.static::$namespace.'\Lang';
+		$refl = new \ReflectionClass($class);
+		$consts = $refl->getConstants();
+		$data = array();
+		foreach ($consts as $key) {
+			$data[$key] = static::get($key);
+		}
+		file_put_contents($file, 'lang = '.json_encode($data).';');
+		return $filename;
+	}
 }
