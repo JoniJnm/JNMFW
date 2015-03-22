c7bb22910dc6bf1368b5a57fd9e566514126249e
JoniJnm <joni@jonijnm.es>
2015-03-22 20:35:16 +0100

    Utilidad getNullDate

--- jnmfw/helpers/HUtils.php
+++ jnmfw/helpers/HUtils.php
@@ -7,6 +7,14 @@ abstract class HUtils {
 		return \sha1(\mt_rand());
 	}
 	
+	static public function getNullDate() {
+		return '0000-00-00';
+	}
+	
+	static public function getNullDateTime() {
+		return '0000-00-00 00:00:00';
+	}
+	
 	static public function formatDate($date) {
 		return \JNMFW\classes\databases\DBFactory::getInstance()->formatDate($date);
 	}
