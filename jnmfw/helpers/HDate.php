08e47f14b28376f812809b68a799c5d8cf68c1be
JoniJnm <JoniJnm@users.noreply.github.com>
2016-08-21 19:23:13 +0200

    HDate changes

--- jnmfw/helpers/HDate.php
+++ jnmfw/helpers/HDate.php
@@ -8,6 +8,7 @@ abstract class HDate
 	 * Devuelve la fecha y hora en formato YYYY-mm-dd
 	 * @param string $date En cualquier formato válido: http://www.php.net/manual/es/datetime.formats.php
 	 * return string fecha formateada
+	 * @return string
 	 */
 	static public function formatDate($date) {
 		return self::format_date($date, 'Y-m-d');
@@ -17,21 +18,36 @@ abstract class HDate
 	 * Devuelve la fecha y hora en formato yyyy-mm-dd hh:mm:ss
 	 * @param string $date En cualquier formato válido: http://www.php.net/manual/es/datetime.formats.php
 	 * return string fecha formateada
+	 * @return string
 	 */
 	static public function formatDateTime($date) {
 		return self::format_date($date, 'Y-m-d H:i:s');
 	}
 
+	static public function getTimestamp($date) {
+		$fech = self::getDate($date);
+		return $fech->getTimestamp();
+	}
+
 	static private function format_date($date, $formato) {
-		if (\is_numeric($date)) {
-			$fech = new \DateTime(null);
-			$fech->setTimestamp($date);
+		$fech = self::getDate($date);
+		//$fech->setTimezone(static::getDateTimeZoneUTC());
+		return $fech->format($formato);
+	}
+
+	/**
+	 * @param int|string $dateStr date as string or timestamp
+	 * @return \DateTime
+	 */
+	static public function getDate($dateStr, $timezone = null) {
+		if (\is_numeric($dateStr)) {
+			$fech = new \DateTime(null, $timezone);
+			$fech->setTimestamp($dateStr);
 		}
 		else {
-			$fech = new \DateTime($date);
+			$fech = new \DateTime($dateStr, $timezone);
 		}
-		$fech->setTimezone(static::getDateTimeZoneUTC());
-		return $fech->format($formato);
+		return $fech;
 	}
 
 	static private function getDateTimeZoneUTC() {
